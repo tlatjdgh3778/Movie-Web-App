75df54ad0ae9812cd93c4cf26b10d0476b844ef8
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-06-18 20:09:08 +0900

    기본 뼈대 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import GlobalStyle from 'style/global';
+import { ThemeProvider } from 'styled-components';
+import theme from 'style/theme';
+import Header from 'components/Header/Header';
+import Main from 'components/Main/Main';
+import Footer from 'components/Footer/Footer';
+import { StylesProvider } from '@material-ui/core';
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
+
   return (
-    <div className="App">
-      hi React !
-    </div>
+    <>
+    <ThemeProvider theme={isDark ? theme.darkMode : theme.lightMode}>
+      <GlobalStyle />
+        <div className="App">
+          <StylesProvider injectFirst>
+            <Header></Header>
+            <Main></Main>
+            <Footer></Footer>
+          </StylesProvider>
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
 
