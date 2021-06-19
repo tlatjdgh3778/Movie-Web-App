f2c7ef480e81d4285a7e7be839482ecc5b2cc103
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-06-19 14:10:27 +0900

    메뉴 버튼 누르면 메뉴 화면 표시

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -12,11 +12,46 @@ const Background = styled.div`
     z-index: 0;
 `;
 
+const MenuContent = styled.div`
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    left: 50%;
+    top: 40%;
+    transform: transition(-50%, -50%);
+    color: ${(props) => props.theme.color.bgColor};
+    border: 1px solid white;
+`;
+
+const CloseBtn = styled.button`
+    position: absolute;
+    right: 0;
+    padding: 1rem;
+    margin: 1rem;
+    color: white;
+`;
+
 const Menu = () => {
-    const { isOpen } = useContext(ModalStateContext).state;
+    const { state, actions } = useContext(ModalStateContext);
+    
+    const closeMenu = () => {
+        actions.setIsOpen(!state.isOpen);
+    }
 
     return(
-        <div>{isOpen?"true":"false"}</div>
+        <>
+            {state.isOpen ? 
+            <Background>
+                <CloseBtn onClick={closeMenu}>X</CloseBtn>
+                <MenuContent>
+                    <div>인기영화</div>
+                    <div>좋은평가</div>
+                    <div>상영중</div>
+                    <div>좋아요</div>
+                    <div>다크모드</div>
+                </MenuContent>
+            </Background> : null }
+        </>
     );
 }
 
