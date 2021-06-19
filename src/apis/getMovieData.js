0a679f3be87891da210acffae71f6f37808039f6
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-06-19 18:41:34 +0900

    api 사용해서 movie data 받아옴

---
+++ src/apis/getMovieData.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const getPopularMovie = async () => {
+    try {
+        const popularData = await fetch(`
+        https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR&page=1`);
+        
+        const popularDataResponse = await popularData.json();
+
+        return popularDataResponse;
+    }catch(error) {
+        console.log(error);
+    }  
+}
+
+const getNowplayingMovie = async () => {
+    try {
+        const nowplayingData = await fetch(`
+        https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=ko-KR&page=1`);
+        
+        const nowplayingDataResponse = await nowplayingData.json();
+
+        return nowplayingDataResponse;
+    }catch(error) {
+        console.log(error);
+    }  
+}
+
+const getTopratedMovie = async () => {
+    try {
+        const topRatedData = await fetch(`
+        https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=ko-KR&page=1`);
+        
+        const topRatedDataResponse = await topRatedData.json();
+
+        return topRatedDataResponse;
+    }catch(error) {
+        console.log(error);
+    }  
+}
+
+const getTrendingMovie = async () => {
+    try {
+        const trendingData = await fetch(`
+        https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&language=ko-KR&page=1`);
+        
+        const trendingDataResponse = await trendingData.json();
+        return trendingDataResponse
+    }catch(error) {
+        console.log(error);
+    }  
+}
+
+export { getPopularMovie, getNowplayingMovie, getTopratedMovie, getTrendingMovie };
