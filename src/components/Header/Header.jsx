0167db138fb22458d1356e4745949cb2e3bf91ad
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-06-23 12:35:36 +0900

    검색 데이터 콘솔에 출력해보기

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import styled from 'styled-components';
 import { ModalStateContext } from 'contexts/modal';
-import { AppBar, Typography, Toolbar, IconButton } from '@material-ui/core';
+import { AppBar, Typography, Toolbar, IconButton, InputBase } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import Brightness4OutlinedIcon from '@material-ui/icons/Brightness4Outlined';
+import { getSearchMovie } from 'apis/getMovieData';
 
 const HeaderContainer = styled.div`
     display: flex;
@@ -15,26 +16,51 @@ const HeaderContainer = styled.div`
     height: 2rem;
 `;
 
+const SearchContainer = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+`;
+
 const Header = () => {
     const { state, actions } = useContext(ModalStateContext);
+    const [value, setValue] = useState("");
 
     const menuOpen = () => {
         actions.setIsOpen(true);
     }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log(value);
+        getSearchMovie(value);
+    };
+
+    const onChange = e => {
+        setValue(e.target.value)
+    };
+
     return(
         <HeaderContainer>
             <IconButton onClick={menuOpen}>
                 <MenuIcon/>
             </IconButton>
             <Typography>Movie Web</Typography>
-            <div>
-                <IconButton>
-                    <SearchIcon/>
-                </IconButton>
-                <IconButton>
+            <SearchContainer>
+                <SearchIcon/>
+                <form onSubmit={onSubmit}>
+                    <InputBase
+                    placeholder="Search.."
+                    onChange={onChange}
+                    value={value}
+                    >
+                    </InputBase>
+                </form>
+                
+                {/* <IconButton>
                     <Brightness4OutlinedIcon/>
-                </IconButton>
-            </div>
+                </IconButton> */}
+            </SearchContainer>
         </HeaderContainer>
     );
 }
