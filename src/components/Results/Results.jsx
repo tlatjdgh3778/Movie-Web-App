4a717d9a801c55117bc86b66b9c19cb584af5e75
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-06-25 16:37:51 +0900

    검색된 영화가 없을 시에 나오는 화면 설정

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -4,29 +4,35 @@ import { ResultContext } from 'contexts/results';
 const Results = () => {
     const { state } = useContext(ResultContext);
 
-    console.log(state.results);
-    
-    return(
-        <>
-        {/* 영화 결과 */}
-        <div>
-                {state.results.length === 0 
-                ? 
-                <div>로딩중</div>
-                :
-                <div>
-                    {state.results.results.map((movie, i) => {
-                        return (
-                            <div key={i}>
-                                <div>{movie.title}</div>
-                                <div>{movie.overview}</div>
-                            </div>
-                        )
-                    })}
-                </div>}
-            </div>
-        </>
-    );
+    if(state.results.total_results === 0){
+        return(
+            <>
+                <div>검색 결과가 없습니다.</div>
+            </>
+        );
+    }else{
+        return(
+            <>
+            {/* 영화 결과 */}
+            <div>
+                    {state.results.length === 0 
+                    ? 
+                    <div>로딩중</div>
+                    :
+                    <div>
+                        {state.results.results.map((movie, i) => {
+                            return (
+                                <div key={i}>
+                                    <div>{movie.title}</div>
+                                    <div>{movie.overview}</div>
+                                </div>
+                            )
+                        })}
+                    </div>}
+                </div>
+            </>
+        );
+    }
 }
 
 export default Results;
