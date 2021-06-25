4f4118960fc805e3fa812642351fdd4e4dba1ec9
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-06-25 23:15:55 +0900

    메인화면 변경

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,22 @@
 import React, { useEffect, useContext } from 'react';
 import styled from 'styled-components';
 import { MovieContext } from 'contexts/movie';
-import { getPopularMovie, getNowplayingMovie, getTopratedMovie, getTrendingMovie } from 'apis/getMovieData';
+import { getPopularMovie, getNowplayingMovie, getTopratedMovie, getTrendingMovie, getMovieDetail } from 'apis/getMovieData';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import * as S from './Main.style';
 
 // MainContainer는 상세 페이지랑.. 목록에도 사용이 가능하지 않나 ?
-const MainContainer = styled.div`
-    border: 1px solid black;
-`;
 
 const Main = () => {
-    const { state, actions } = useContext(MovieContext);
+    const { state } = useContext(MovieContext);
+    const { setPopular, setNowPlaying, setTopRated, setTrend, setDetail } = useContext(MovieContext).actions;
 
+    const history = useHistory();
+
+    const null_img = "http://collaboparty1004.cafe24.com/xe/files/attach/images/139/483/d8c711f2e76e6be056d911b8fbed47fd.jpg";
+    const posterImg = "https://image.tmdb.org/t/p/original/";
+    const backdropImg = "https://image.tmdb.org/t/p/original/";
 
     useEffect(() => {
         const fetchData = async () => {
@@ -19,27 +25,39 @@ const Main = () => {
             const rated = await getTopratedMovie();
             const trend = await getTrendingMovie();
     
-            actions.setPopular(popular);
-            actions.setNowPlaying(playing);
-            actions.setTopRated(rated);
-            actions.setTrend(trend);
+            await setPopular(popular);
+            await setNowPlaying(playing);
+            await setTopRated(rated);
+            await setTrend(trend);
+
+            const detail = await getMovieDetail(trend.results[0].id);
+            await setDetail(detail);
+            console.log(detail);
         }
         fetchData();
     }, []);
 
+    const getDetail = async () => {
+        const detail = await getMovieDetail(state.trend.results[0].id);
+        setDetail(detail);
+
+        history.push(`/Detail/${state.trend.results[0].id}`);
+    }
+    
     return(
-        <MainContainer>
+        <S.MainContainer>
             {/* 트렌드 영화 컴포넌트 */}
             <div>
                 {state.trend.length === 0 
                 ? 
                 <div>로딩중</div>
                 :
-                <div>
-                    <div>{state.trend.results[5].title}</div>
-                    <div>{state.trend.results[5].overview}</div>
-                    <div>{state.trend.results[5].id}</div>
-                </div>}
+                    <S.MainMovieContainer backdropPath={backdropImg + state.detail.backdrop_path}
+                    >
+                        <S.Title>{state.detail.title}</S.Title>
+                        <S.Contet>{state.detail.tagline}</S.Contet>
+                        <button onClick={getDetail}>더보기</button>
+                    </S.MainMovieContainer>}
             </div>
             {/* 인기있는 영화 */}
             <div>
@@ -92,7 +110,7 @@ const Main = () => {
                     })}
                 </div>}
             </div>
-        </MainContainer>
+        </S.MainContainer>
     );
 }
 
