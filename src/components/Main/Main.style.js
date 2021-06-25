55a2d7393f7b9a302808488e53a5c2edee0ea8de
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-06-25 22:55:58 +0900

    메인 화면 디자인

---
+++ src/components/Main/Main.style.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { Typography } from '@material-ui/core';
+
+const MainContainer = styled.div`
+    border: 1px solid black;
+`;
+
+const MainMovieContainer = styled.div`
+    /* width: 100%; */
+    height: 40vw;
+    background-image: url(${({ backdropPath }) => backdropPath});
+    background-size: cover;
+    background-position: center center;
+    background-repeat: no-repeat;
+    opacity: 1;
+    transition: all 420ms ease 0s;
+    /* display: inline-block; */
+    /* position: absolute; */
+    /* inset: 0px; */
+`;
+
+const Title = styled.div`
+    font-size: 5vw;
+    padding-top: 10vw;
+    padding-left: 5vw;
+    font-weight: 700;
+    color: ${(props) => props.theme.color.fontColor};
+
+`;
+const Contet = styled.div`
+    font-size: 1.5vw;
+    padding-left: 5vw;
+`;
+
+export { MainMovieContainer, MainContainer, Title, Contet };
