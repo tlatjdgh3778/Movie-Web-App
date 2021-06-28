4c914f865dd8795d180ccb2ed48f7b87bb8de3eb
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-06-28 20:38:33 +0900

    공통 컴포넌트 스타일

---
+++ src/style/componentstyle.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import { Button, GridList, GridListTile, GridListTileBar } from '@material-ui/core';
+
+// 영화 리스트 재활용 가능..
+const ListContainer = styled.div`
+    background-color: ${(props) => props.theme.color.listBgColor};
+`;
+const ListHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 1vw;
+`;
+const ListTitle = styled.div`
+    color: ${(props) => props.theme.color.fontColor};
+`;
+const ViewAll = styled.div`
+    cursor: pointer;
+    color: ${(props) => props.theme.color.fontColor};
+    
+    &:hover {
+        transition: all 0.2s ease;
+        color: ${(props) => props.theme.color.fontHoverColor};
+    }
+`;
+const MovieContainer = styled.div`
+    padding: 1vw;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    overflow: hidden;
+`;
+const MovieList = styled(GridList)`
+    flex-wrap: nowrap;
+`;
+
+const Movie = styled(GridListTile)`
+    cursor: pointer;
+`;
+const MovieTitle = styled(GridListTileBar)`
+    color: white;
+`;
+// 여기까지
+
+export { ListContainer, ListHeader, ListTitle, ViewAll, MovieContainer, MovieList, Movie, MovieTitle };
