be695e2551cb956165eac558e0638bbf156fa880
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-06-30 20:26:36 +0900

    Main 페이지 세분화

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,13 +3,41 @@ import { MovieContext } from 'contexts/movie';
 import { getPopularMovie, getNowplayingMovie, getTopratedMovie, getTrendingMovie, getMovieDetail, getMovieCredit, getMovieRecommendation, getMovieVideo } from 'apis/getMovieData';
 import { useHistory } from 'react-router-dom';
 import * as S from './Main.style';
-import * as GS from 'style/componentstyle';
 import { useTheme } from '@material-ui/core/styles';
-import { GridList, GridListTile, GridListTileBar, useMediaQuery } from '@material-ui/core';
+import { useMediaQuery } from '@material-ui/core';
+import MainIntro from './MainIntro/MainIntro';
+import MainPopular from './MainPopular/MainPopular';
+import MainNowPlaying from './MainNowPlaying/MainNowPlaying';
+import MainTopRated from './MainTopRated/MainTopRated';
+
+
 // MainContainer는 상세 페이지랑.. 목록에도 사용이 가능하지 않나 ?
 
 const Main = () => {
+    const { setPopular, setNowPlaying, setTopRated, setTrend, setDetail, setCredit, setRecommendation, setVideo } = useContext(MovieContext).actions;
+    const history = useHistory();
     const theme = useTheme();
+
+    const random = Math.floor(Math.random() * 10);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const popular = await getPopularMovie();
+            const playing = await getNowplayingMovie();
+            const rated = await getTopratedMovie();
+            const trend = await getTrendingMovie();
+    
+            await setPopular(popular);
+            await setNowPlaying(playing);
+            await setTopRated(rated);
+            await setTrend(trend);
+
+            const detail = await getMovieDetail(trend.results[random].id);
+            await setDetail(detail);
+        }
+        fetchData();
+    }, []);
+
     const mobileMatches = useMediaQuery(theme.breakpoints.values.mobile);
     const tabletMatches = useMediaQuery(theme.breakpoints.values.tablet);
     const laptopMatches = useMediaQuery(theme.breakpoints.values.laptop);
@@ -30,32 +58,6 @@ const Main = () => {
         }
     }
 
-    const { state } = useContext(MovieContext);
-    const { setPopular, setNowPlaying, setTopRated, setTrend, setDetail, setCredit, setRecommendation, setVideo } = useContext(MovieContext).actions;
-    const history = useHistory();
-    const random = Math.floor(Math.random() * 10);
-
-    const backdropImg = "https://image.tmdb.org/t/p/original/";
-    const nullImg = "http://collaboparty1004.cafe24.com/xe/files/attach/images/139/483/d8c711f2e76e6be056d911b8fbed47fd.jpg";
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const popular = await getPopularMovie();
-            const playing = await getNowplayingMovie();
-            const rated = await getTopratedMovie();
-            const trend = await getTrendingMovie();
-    
-            await setPopular(popular);
-            await setNowPlaying(playing);
-            await setTopRated(rated);
-            await setTrend(trend);
-
-            const detail = await getMovieDetail(trend.results[random].id);
-            await setDetail(detail);
-        }
-        fetchData();
-    }, []);
-
     const getDetail = async (id) => {
         const detail = await getMovieDetail(id);
         const credit = await getMovieCredit(id);
@@ -71,98 +73,10 @@ const Main = () => {
 
     return(
         <S.MainContainer>
-            {/* 트렌드 영화 컴포넌트 */}
-            <div>
-                {state.trend.length === 0 
-                ? 
-                <div>로딩중</div>
-                :
-                    <S.MainMovieContainer backdropPath={backdropImg + state.detail.backdrop_path}
-                    >
-                        <div>
-                            <S.Title>{state.detail.title}</S.Title>
-                            <S.Tagline>{state.detail.tagline}</S.Tagline>
-                            <S.MoreBtn id={state.detail.id} onClick={e=>{
-                                getDetail(e.currentTarget.id);
-                            }}>더보기</S.MoreBtn>
-                        </div>
-                    </S.MainMovieContainer>}
-            </div>
-            {/* 인기있는 영화 */}
-            <GS.ListContainer>
-                <GS.ListHeader>
-                    <GS.ListTitle>인기있는 영화</GS.ListTitle>
-                    <GS.ViewAll>전체 보기</GS.ViewAll>
-                </GS.ListHeader>
-                {state.popular.length === 0 
-                ? 
-                <div>로딩중</div>
-                :
-                <GS.MovieContainer>
-                    <GS.List cellHeight={'auto'} spacing={20} cols={getGridListCols()}>
-                        {state.popular.results.map((movie, i)=> {
-                            return (
-                            <GridListTile id={movie.id} key={i} onClick={e=> {
-                                getDetail(e.currentTarget.id)
-                            }}>
-                                <img src={movie.poster_path === null ? nullImg : (backdropImg + movie.poster_path)}></img>
-                                <GridListTileBar title={movie.title}></GridListTileBar>
-                            </GridListTile>)
-                        })}
-                    </GS.List>
-                </GS.MovieContainer>
-                }
-            </GS.ListContainer>
-            {/* 상영중인 영화 */}
-            <GS.ListContainer>
-                <GS.ListHeader>
-                    <GS.ListTitle>상영중인 영화</GS.ListTitle>
-                    <GS.ViewAll>전체 보기</GS.ViewAll>
-                </GS.ListHeader>
-                    {state.nowPlaying.length === 0 
-                    ? 
-                    <div>로딩중</div>
-                    : 
-                    <GS.MovieContainer>
-                        <GS.List cellHeight={'auto'} spacing={20} cols={getGridListCols()}>
-                        {state.nowPlaying.results.map((movie, i)=> {
-                            return (
-                            <GridListTile id={movie.id} key={i} onClick={e=> {
-                                getDetail(e.currentTarget.id)
-                            }}>
-                                <img src={movie.poster_path === null ? nullImg : (backdropImg + movie.poster_path)}></img>
-                                <GridListTileBar title={movie.title}></GridListTileBar>
-                            </GridListTile>)
-                        })}
-                        </GS.List>
-                    </GS.MovieContainer>
-                    }
-            </GS.ListContainer>
-            {/* 별점높은 영화 */}
-            <GS.ListContainer>
-                <GS.ListHeader>
-                    <GS.ListTitle>별점높은 영화</GS.ListTitle>
-                    <GS.ViewAll>전체 보기</GS.ViewAll>
-                </GS.ListHeader>
-                {state.topRated.length === 0 
-                ? 
-                <div>로딩중</div>
-                :
-                <GS.MovieContainer>
-                    <GS.List cellHeight={'auto'} spacing={20} cols={getGridListCols()}>
-                        {state.topRated.results.map((movie, i)=> {
-                            return (
-                            <GridListTile id={movie.id} key={i} onClick={e=> {
-                                getDetail(e.currentTarget.id)
-                            }}>
-                                <img src={movie.poster_path === null ? nullImg : (backdropImg + movie.poster_path)}></img>
-                                <GridListTileBar title={movie.title}></GridListTileBar>
-                            </GridListTile>)
-                        })}
-                    </GS.List>
-                </GS.MovieContainer>
-                }
-            </GS.ListContainer>
+            <MainIntro getDetail={getDetail}></MainIntro>
+            <MainPopular getDetail={getDetail} getGridListCols={getGridListCols}></MainPopular>
+            <MainNowPlaying getDetail={getDetail} getGridListCols={getGridListCols}></MainNowPlaying>
+            <MainTopRated getDetail={getDetail} getGridListCols={getGridListCols}></MainTopRated>
         </S.MainContainer>
     );
 }
