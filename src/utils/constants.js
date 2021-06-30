da9ea02f5c7fe4e548c326b12c38a5481b5efd02
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-06-30 19:37:50 +0900

    전역 변수 관리

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+const backdropImg = "https://image.tmdb.org/t/p/original/";
+const nullImg = "http://collaboparty1004.cafe24.com/xe/files/attach/images/139/483/d8c711f2e76e6be056d911b8fbed47fd.jpg";
+const youtubeLink = "https://www.youtube.com/watch?v=";
+const thumbnail ="https://i.ytimg.com/vi/";
+
+export { backdropImg, nullImg, youtubeLink, thumbnail };
