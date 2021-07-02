8494d015189e13e6b3fd16dbb8018f37968fd40f
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-02 18:52:57 +0900

    인기 영화 목록 페이지

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,21 +1,47 @@
 import React, { useContext } from 'react';
 import { MovieContext } from 'contexts/movie';
+import { useTheme } from '@material-ui/core/styles';
+import { GridListTile, GridListTileBar, useMediaQuery } from '@material-ui/core';
+import * as GS from 'style/componentstyle';
+import { backdropImg, nullImg } from 'utils/constants';
 
 const Popular = () => {
-    const { state, actions } = useContext(MovieContext);
+    const { popular } = useContext(MovieContext).state;
+    const theme = useTheme();
+
+    const mobileMatches = useMediaQuery(theme.breakpoints.values.mobile);
+    const tabletMatches = useMediaQuery(theme.breakpoints.values.tablet);
+    const laptopMatches = useMediaQuery(theme.breakpoints.values.laptop);
+    const desktopMatches = useMediaQuery(theme.breakpoints.values.desktop);
+
+    const getGridListCols = () => {
+        if(mobileMatches){
+            return 2;
+        }
+        if(tabletMatches){
+            return 2;
+        }
+        if(laptopMatches){
+            return 3;
+        }
+        if(desktopMatches){
+            return 4;
+        }
+    }
 
     return(
         <>
-            <div>
-                {state.popular.results.map((movie, i) => {
-                    return (
-                        <div key={i}>
-                            <div>{movie.title}</div>
-                            <div>{movie.overview}</div>
-                        </div>
-                    )
-                })}
-            </div>
+            <GS.ListMovieTitle>인기 영화 목록</GS.ListMovieTitle>
+            <GS.MovieContainer>        
+                <GS.ListMovie cellHeight={'auto'} cols={getGridListCols()} spacing={30}> 
+                    {popular.results.map((movie, i) => (
+                        <GridListTile key={i} id={movie.id}>
+                            <img src={movie.poster_path === null ? nullImg : (backdropImg + movie.poster_path)}></img>
+                            <GridListTileBar title={movie.title}></GridListTileBar>
+                        </GridListTile>
+                    ))}
+                </GS.ListMovie>
+            </GS.MovieContainer>
         </>
     );
 }
