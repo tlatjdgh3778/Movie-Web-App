5fc3bfd35f4f87a420c1486f18ada5dc419b75a6
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-02 21:56:27 +0900

    영화 검색 결과 페이지

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,10 +1,33 @@
 import React, { useContext } from 'react';
 import { ResultContext } from 'contexts/results';
+import { MovieContext } from 'contexts/movie';
+import { useHistory } from 'react-router-dom';
+import { getMovieDetail, getMovieCredit, getMovieRecommendation, getMovieVideo } from 'apis/getMovieData';
+import { GridListTile, GridListTileBar } from '@material-ui/core';
+import * as GS from 'style/componentstyle';
+import { backdropImg, nullImg } from 'utils/constants';
 
-const Results = () => {
-    const { state } = useContext(ResultContext);
+const Results = ({ getGridListCols }) => {
+    const { results, searchValue } = useContext(ResultContext).state;
+    const { setDetail, setCredit, setRecommendation, setVideo } = useContext(MovieContext).actions;
 
-    if(state.results.total_results === 0){
+    const history = useHistory();
+
+    const getDetail = async (id) => {
+        const detail = await getMovieDetail(id);
+        const credit = await getMovieCredit(id);
+        const recommendation = await getMovieRecommendation(id);
+        const video = await getMovieVideo(id);
+        
+        await setDetail(detail);
+        await setCredit(credit);
+        await setRecommendation(recommendation);
+        await setVideo(video);
+        history.push(`/Detail/${id}`);
+    };
+
+    console.log(results)
+    if(results.total_results === 0){
         return(
             <>
                 <div>검색 결과가 없습니다.</div>
@@ -14,22 +37,19 @@ const Results = () => {
         return(
             <>
             {/* 영화 결과 */}
-            <div>
-                    {state.results.length === 0 
-                    ? 
-                    <div>로딩중</div>
-                    :
-                    <div>
-                        {state.results.results.map((movie, i) => {
-                            return (
-                                <div key={i}>
-                                    <div>{movie.title}</div>
-                                    <div>{movie.overview}</div>
-                                </div>
-                            )
-                        })}
-                    </div>}
-                </div>
+            <GS.ListMovieTitle>"{searchValue}" 검색 결과</GS.ListMovieTitle>
+            <GS.MovieContainer>        
+                <GS.ListMovie cellHeight={'auto'} cols={getGridListCols()} spacing={30}> 
+                    {results.results.map((movie, i) => (
+                        <GridListTile onClick={e => {
+                            getDetail(e.currentTarget.id)
+                        }} key={i} id={movie.id}>
+                            <img src={movie.poster_path === null ? nullImg : (backdropImg + movie.poster_path)}></img>
+                            <GridListTileBar title={movie.title}></GridListTileBar>
+                        </GridListTile>
+                    ))}
+                </GS.ListMovie>
+            </GS.MovieContainer>
             </>
         );
     }
