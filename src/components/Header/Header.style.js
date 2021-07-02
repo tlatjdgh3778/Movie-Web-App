4fd52792f2ab05c7630af95f97b07af990b914f5
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-02 22:48:00 +0900

    헤더 디자인

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -1,4 +1,37 @@
 import styled from 'styled-components';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import { Typography, IconButton, InputBase } from '@material-ui/core';
 
+const HeaderContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem;
+    height: 2rem;
+`;
 
-export {};
+const SearchContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    border-radius: 5px;
+    background-color: #212121;
+    width: 12rem;
+`;
+
+const CustomMenuIcon = styled(MenuIcon)`
+    color: ${(props) => props.theme.color.fontColor};
+`;
+
+const CustomInputBase = styled(InputBase)`
+    padding: 0.3rem 0;
+    color: ${(props) => props.theme.color.fontColor};
+`;
+
+const CustomSearchIcon = styled(SearchIcon)`
+    padding: 0 0.5rem;
+    color: ${(props) => props.theme.color.fontColor};
+`;
+
+export { HeaderContainer, SearchContainer, CustomMenuIcon, CustomSearchIcon, CustomInputBase };
