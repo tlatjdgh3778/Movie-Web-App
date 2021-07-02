9fb5ae108d40d77657e4ea39084f82830ddd6577
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-02 21:22:55 +0900

    영화 누르면 디테일 페이지로 이동

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -3,9 +3,27 @@ import { MovieContext } from 'contexts/movie';
 import { GridListTile, GridListTileBar } from '@material-ui/core';
 import * as GS from 'style/componentstyle';
 import { backdropImg, nullImg } from 'utils/constants';
+import { useHistory } from 'react-router-dom';
+import { getMovieDetail, getMovieCredit, getMovieRecommendation, getMovieVideo } from 'apis/getMovieData';
 
 const Popular = ({ getGridListCols }) => {
     const { popular } = useContext(MovieContext).state;
+    const { setDetail, setCredit, setRecommendation, setVideo } = useContext(MovieContext).actions;
+
+    const history = useHistory();
+
+    const getDetail = async (id) => {
+        const detail = await getMovieDetail(id);
+        const credit = await getMovieCredit(id);
+        const recommendation = await getMovieRecommendation(id);
+        const video = await getMovieVideo(id);
+        
+        await setDetail(detail);
+        await setCredit(credit);
+        await setRecommendation(recommendation);
+        await setVideo(video);
+        history.push(`/Detail/${id}`);
+    };
 
     return(
         <>
@@ -13,7 +31,9 @@ const Popular = ({ getGridListCols }) => {
             <GS.MovieContainer>        
                 <GS.ListMovie cellHeight={'auto'} cols={getGridListCols()} spacing={30}> 
                     {popular.results.map((movie, i) => (
-                        <GridListTile key={i} id={movie.id}>
+                        <GridListTile onClick={e => {
+                            getDetail(e.currentTarget.id)
+                        }} key={i} id={movie.id}>
                             <img src={movie.poster_path === null ? nullImg : (backdropImg + movie.poster_path)}></img>
                             <GridListTileBar title={movie.title}></GridListTileBar>
                         </GridListTile>
--- src/components/TopRated/TopRated.jsx
+++ src/components/TopRated/TopRated.jsx
@@ -3,17 +3,36 @@ import { MovieContext } from 'contexts/movie';
 import { GridListTile, GridListTileBar } from '@material-ui/core';
 import * as GS from 'style/componentstyle';
 import { backdropImg, nullImg } from 'utils/constants';
+import { useHistory } from 'react-router-dom';
+import { getMovieDetail, getMovieCredit, getMovieRecommendation, getMovieVideo } from 'apis/getMovieData';
 
 const TopRated = ({ getGridListCols }) => {
     const { topRated } = useContext(MovieContext).state;
+    const { setDetail, setCredit, setRecommendation, setVideo } = useContext(MovieContext).actions;
 
+    const history = useHistory();
+
+    const getDetail = async (id) => {
+        const detail = await getMovieDetail(id);
+        const credit = await getMovieCredit(id);
+        const recommendation = await getMovieRecommendation(id);
+        const video = await getMovieVideo(id);
+        
+        await setDetail(detail);
+        await setCredit(credit);
+        await setRecommendation(recommendation);
+        await setVideo(video);
+        history.push(`/Detail/${id}`);
+    };
     return(
         <>
             <GS.ListMovieTitle>별점이 높은 영화 목록</GS.ListMovieTitle>
             <GS.MovieContainer>        
                 <GS.ListMovie cellHeight={'auto'} cols={getGridListCols()} spacing={30}> 
                     {topRated.results.map((movie, i) => (
-                        <GridListTile key={i} id={movie.id}>
+                        <GridListTile onClick={e => {
+                            getDetail(e.currentTarget.id)
+                        }} key={i} id={movie.id}>
                             <img src={movie.poster_path === null ? nullImg : (backdropImg + movie.poster_path)}></img>
                             <GridListTileBar title={movie.title}></GridListTileBar>
                         </GridListTile>
