8cdd951020a7fa10b903cda10c3db7a790952a73
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-03 19:08:39 +0900

    페이지 이동 시 스크롤 맨 위로 오게

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollTop from 'hooks/ScrollTop'; 
 
 ReactDOM.render(
   <BrowserRouter>
+  <ScrollTop />
     <App />
   </BrowserRouter>,
   document.getElementById('root')
