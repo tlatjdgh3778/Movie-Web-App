18fa1acfc104af66e08d63438bc7816eaf7d713c
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-03 20:44:17 +0900

    페이지 이동 시 스크롤 맨 위로

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import ScrollTop from 'hooks/ScrollTop'; 
+import ScrollTop from 'components/Scroll/ScrollTop'; 
 
 ReactDOM.render(
   <BrowserRouter>
