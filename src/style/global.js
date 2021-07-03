762dff83f428ba4b789ea3460196f36c221fb243
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-03 18:14:35 +0900

    스크롤 바 디자인 변경

--- src/style/global.js
+++ src/style/global.js
@@ -1,10 +1,41 @@
 import { createGlobalStyle } from 'styled-components';
 
+
 const GlobalStyle = createGlobalStyle`
+:root {
+    --scrollbarThumb-color: #666;
+    --scrollbarBtn-color: #666;
+    --scrollbarTrack-color: black;
+    --scrollbarHover-color: #121212;
+}
 * {
     font-family: 'Noto Sans KR', 'Roboto', sans-serif !important;
     margin: 0;
     padding: 0;
+
+    &::-webkit-scrollbar {
+        width: 15px;
+        height: 10px;
+    }
+    &::-webkit-scrollbar-thumb {
+        background-color: var(--scrollbarThumb-color); 
+        border-radius: 10px;
+
+        &:hover {
+            background-color: var(--scrollbarHover-color);
+        }
+        &:focus {
+            background-color: var(--scrollbarHover-color);
+        }
+    }
+    &::-webkit-scrollbar-track {
+        background-color: var(--scrollbarTrack-color);
+        border-radius: 10px;
+    }
+
+    &::-webkit-scrollbar-button { 
+        background-color: var(--scrollbarBtn-color); 
+    }
 }
 
 body {
@@ -15,6 +46,9 @@ body {
 
 img {
     border-radius: 5px;
+}
+a {
+    text-decoration: none;
 }
 
 .MuiGridListTile-imgFullWidth {
@@ -41,6 +75,8 @@ img {
 .MuiListSubheader-root {
     color: ${(props) => props.theme.color.fontColor};
 }
+
+
 `;
 
 export default GlobalStyle; 
