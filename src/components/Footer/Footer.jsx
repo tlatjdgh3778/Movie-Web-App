28a68372de3dac46ed8a41f7558be8c5dc48a5a0
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-04 15:40:54 +0900

    스타일 파일 분리

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const FooterContainer = styled.div`
-    border: 1px solid black;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 2rem;
-`;
+import * as S from './Footer.style';
 
 const Footer = () => {
     return(
-        <FooterContainer>
+        <S.FooterContainer>
             <div>Made By SeongHo Shim</div>
-        </FooterContainer>
+        </S.FooterContainer>
     );
 }
 
---
+++ src/components/Footer/Footer.style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const FooterContainer = styled.div`
+    border: 1px solid black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 2rem;
+`;
+
+export { FooterContainer };
