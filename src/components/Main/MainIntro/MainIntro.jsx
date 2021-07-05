8605be5b420b8418f79339f00cf4a0aa9c33c57b
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-05 21:20:54 +0900

    MainIntro

--- src/components/Main/MainIntro/MainIntro.jsx
+++ src/components/Main/MainIntro/MainIntro.jsx
@@ -1,32 +1,11 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { MovieContext } from 'contexts/movie';
-import { getPopularMovie, getNowplayingMovie, getTopratedMovie, getTrendingMovie, getMovieDetail } from 'apis/getMovieData';
 import * as S from './MainIntro.style';
 import { backdropImg } from 'utils/constants';
 
 const MainIntro = ({ getDetail }) => {
     const { detail } = useContext(MovieContext).state;
-    const { setPopular, setNowPlaying, setTopRated, setTrend, setDetail } = useContext(MovieContext).actions;
-    const random = Math.floor(Math.random() * 10);
     
-    useEffect(() => {
-        const fetchData = async () => {
-            const popular = await getPopularMovie();
-            const playing = await getNowplayingMovie();
-            const rated = await getTopratedMovie();
-            const trend = await getTrendingMovie();
-    
-            await setPopular(popular);
-            await setNowPlaying(playing);
-            await setTopRated(rated);
-            await setTrend(trend);
-
-            const detail = await getMovieDetail(trend.results[random].id);
-            await setDetail(detail);
-        }
-        fetchData();
-    }, [setTrend, setTopRated, setPopular, setNowPlaying, setDetail]);
-
     return(
         // MainIntro
             <div>
