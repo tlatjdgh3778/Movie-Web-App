f7cf3fdbbca68aa89f8205a728d6f406407cdf0e
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-25 18:51:35 +0900

    refactor : es6 destructuring 으로 중복 코드 제거

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,42 +1,49 @@
-import React, { useContext } from 'react';
-import { ModalStateContext } from 'contexts/modal';
-import { ModeContext } from 'contexts/mode';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import * as S from './Menu.style';
+import { connect } from 'react-redux';
+import { changeMode } from 'store/modules/mode';
+import { toggleModal } from 'store/modules/modal';
 
-const Menu = () => {
-    const { state, actions } = useContext(ModalStateContext);
-    const { isDark } = useContext(ModeContext).state;
-    const { setIsDark } = useContext(ModeContext).actions;
-
-    const closeMenu = () => {
-        actions.setIsOpen(!state.isOpen);
-    }
-
-    const changeMode = () => {
-        setIsDark(!isDark);
-    }
+const Menu = ({ isDark, isOpen, changeMode, toggleModal }) => {
 
     return(
         <>
-            {state.isOpen ? 
+            {isOpen ? 
             <S.Background>
                 <S.ModeIconButton onClick={changeMode}>
                     {isDark ? <S.LightMode /> : <S.DarkMode /> }
                 </S.ModeIconButton>
-                <S.CloseIconButton onClick={closeMenu}>
+                <S.CloseIconButton onClick={toggleModal}>
                     <S.CloseBtn />
                 </S.CloseIconButton>
                 <S.MenuContent>
-                    <NavLink to="/" onClick={closeMenu}><S.MenuItem>메인화면</S.MenuItem></NavLink>
-                    <NavLink to="/Popular" onClick={closeMenu}><S.MenuItem>인기영화</S.MenuItem></NavLink>
-                    <NavLink to="/TopRated" onClick={closeMenu}><S.MenuItem>좋은평가</S.MenuItem></NavLink>
-                    <NavLink to="/NowPlaying" onClick={closeMenu}><S.MenuItem>상영중</S.MenuItem></NavLink>
-                    <NavLink to="/Likes" onClick={closeMenu}><S.MenuItem>좋아요</S.MenuItem></NavLink>
+                    <NavLink to="/" onClick={toggleModal}><S.MenuItem>메인화면</S.MenuItem></NavLink>
+                    <NavLink to="/Popular" onClick={toggleModal}><S.MenuItem>인기영화</S.MenuItem></NavLink>
+                    <NavLink to="/TopRated" onClick={toggleModal}><S.MenuItem>좋은평가</S.MenuItem></NavLink>
+                    <NavLink to="/NowPlaying" onClick={toggleModal}><S.MenuItem>상영중</S.MenuItem></NavLink>
+                    <NavLink to="/Likes" onClick={toggleModal}><S.MenuItem>좋아요</S.MenuItem></NavLink>
                 </S.MenuContent>
             </S.Background> : null }
         </>
     );
 }
 
-export default Menu;
+const mapStateToProps = ({ mode, modal }) => {
+    return {
+        isDark: mode.isDark,
+        isOpen: modal.isOpen,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        changeMode: () => dispatch(changeMode()),
+        toggleModal: () => dispatch(toggleModal()),
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Menu);
