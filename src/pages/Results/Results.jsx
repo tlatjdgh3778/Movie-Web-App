9617ce92da99a5da96bb811a42740d82d49bf9ea
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-25 20:00:13 +0900

    refactor: context api 에서 redux 적용

--- src/pages/Results/Results.jsx
+++ src/pages/Results/Results.jsx
@@ -1,12 +1,11 @@
-import React, { useContext } from 'react';
-import { ResultContext } from 'contexts/results';
+import React from 'react';
 import * as GS from 'style/componentstyle';
 import { ResultList } from 'components/Results/index';
+import { connect } from 'react-redux';
 
-const Results = ({ getGridListCols }) => {
-    const { results, searchValue } = useContext(ResultContext).state;
+const Results = ({ getGridListCols, searchValue, searchResults }) => {
 
-    if(results.total_results === 0){
+    if(searchResults.total_results === 0){
         return(
             <>
                 <GS.ExceptionMsg>검색 결과가 없습니다.</GS.ExceptionMsg>
@@ -25,4 +24,13 @@ const Results = ({ getGridListCols }) => {
     }
 }
 
-export default Results;
+const mapStateToProps = ({ search }) => {
+    return {
+        searchResults: search.searchResults,
+        searchValue: search.searchValue
+    }
+}
+
+export default connect(
+    mapStateToProps
+)(Results);
