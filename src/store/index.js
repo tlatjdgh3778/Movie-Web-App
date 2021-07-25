b8a4fb98121838f9705adb36fc9c341adefef6ab
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-25 20:00:45 +0900

    feat : install thunk middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,15 @@
 // define store
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import rootReducer from './modules';
 
-const store = createStore(rootReducer);
+const middleware = [thunk]
+const store = createStore(
+    rootReducer, 
+    composeWithDevTools(
+        applyMiddleware(...middleware)
+    )
+);
 
 export default store;
