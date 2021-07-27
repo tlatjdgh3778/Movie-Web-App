9fd87e918e309294bbd0146a0b55561d653db0b4
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-27 17:37:56 +0900

    refactor : connect 대신 redux + hook 으로 상태 관리

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,49 +1,35 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import * as S from './Menu.style';
-import { connect } from 'react-redux';
 import { changeMode } from 'store/modules/mode';
 import { toggleModal } from 'store/modules/modal';
+import { useSelector, useDispatch } from 'react-redux';
 
-const Menu = ({ isDark, isOpen, changeMode, toggleModal }) => {
+const Menu = () => {
+    const dispatch = useDispatch();
+    const isDark = useSelector(({ mode }) => mode.isDark);
+    const isOpen = useSelector(({ modal }) => modal.isOpen);
 
     return(
         <>
             {isOpen ? 
             <S.Background>
-                <S.ModeIconButton onClick={changeMode}>
+                <S.ModeIconButton onClick={() => dispatch(changeMode())}>
                     {isDark ? <S.LightMode /> : <S.DarkMode /> }
                 </S.ModeIconButton>
-                <S.CloseIconButton onClick={toggleModal}>
+                <S.CloseIconButton onClick={() => dispatch(toggleModal())}>
                     <S.CloseBtn />
                 </S.CloseIconButton>
                 <S.MenuContent>
-                    <NavLink to="/" onClick={toggleModal}><S.MenuItem>메인화면</S.MenuItem></NavLink>
-                    <NavLink to="/Popular" onClick={toggleModal}><S.MenuItem>인기영화</S.MenuItem></NavLink>
-                    <NavLink to="/TopRated" onClick={toggleModal}><S.MenuItem>좋은평가</S.MenuItem></NavLink>
-                    <NavLink to="/NowPlaying" onClick={toggleModal}><S.MenuItem>상영중</S.MenuItem></NavLink>
-                    <NavLink to="/Likes" onClick={toggleModal}><S.MenuItem>좋아요</S.MenuItem></NavLink>
+                    <NavLink to="/" onClick={() => dispatch(toggleModal())}><S.MenuItem>메인화면</S.MenuItem></NavLink>
+                    <NavLink to="/Popular" onClick={() => dispatch(toggleModal())}><S.MenuItem>인기영화</S.MenuItem></NavLink>
+                    <NavLink to="/TopRated" onClick={() => dispatch(toggleModal())}><S.MenuItem>좋은평가</S.MenuItem></NavLink>
+                    <NavLink to="/NowPlaying" onClick={() => dispatch(toggleModal())}><S.MenuItem>상영중</S.MenuItem></NavLink>
+                    <NavLink to="/Likes" onClick={() => dispatch(toggleModal())}><S.MenuItem>좋아요</S.MenuItem></NavLink>
                 </S.MenuContent>
             </S.Background> : null }
         </>
     );
 }
 
-const mapStateToProps = ({ mode, modal }) => {
-    return {
-        isDark: mode.isDark,
-        isOpen: modal.isOpen,
-    }
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        changeMode: () => dispatch(changeMode()),
-        toggleModal: () => dispatch(toggleModal()),
-    }
-}
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Menu);
+export default Menu;
