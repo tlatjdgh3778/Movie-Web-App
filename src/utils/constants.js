439d8ddbfaa1f1ff34e4a6cc6b7f0cd2af93d87e
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-27 17:56:33 +0900

    style : 사용하지 않는 변수 삭제

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,9 @@
+export const API_KEY = process.env.REACT_APP_API_KEY;
+
 const backdropImg = "https://image.tmdb.org/t/p/original/";
 const nullImg = "http://collaboparty1004.cafe24.com/xe/files/attach/images/139/483/d8c711f2e76e6be056d911b8fbed47fd.jpg";
 const youtubeLink = "https://www.youtube.com/watch?v=";
 const thumbnail ="https://i.ytimg.com/vi/";
-const random = Math.floor(Math.random() * 10);
 
-export { backdropImg, nullImg, youtubeLink, thumbnail, random };
+
+export { backdropImg, nullImg, youtubeLink, thumbnail };
