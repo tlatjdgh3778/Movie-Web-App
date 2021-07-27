e6356eb3e8b365377f702009df468621fd8e600c
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-27 15:14:56 +0900

    feat : create loading component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactLoading from 'react-loading';
+
+const Loading = () => {
+    return (
+        <>
+        <ReactLoading type={"spin"} color="#fff" className="loading"/>
+        </>
+    )
+}
+
+export default Loading;
