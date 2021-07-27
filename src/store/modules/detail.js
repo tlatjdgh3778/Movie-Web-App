21be9fcc6d95f6d7d0319bb8ba8507c47a341c88
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-27 15:17:04 +0900

    feat : 영화 deatil을 받아올 때 credit,  video 등도 같이 받아오기

--- src/store/modules/detail.js
+++ src/store/modules/detail.js
@@ -8,34 +8,80 @@ export const FETCH_DETAIL_REQUEST = 'detail/FETCH_DETAIL_REQUEST'
 export const FETCH_DETAIL_SUCCESS = 'detail/FETCH_DETAIL_SUCCESS' 
 export const FETCH_DETAIL_FAILURE = 'detail/FETCH_DETAIL_FAILURE' 
 
-export const fetchDetailRequest = () => {
+export const fetchDetailRequest = (filter) => {
     return {
-        type: FETCH_DETAIL_REQUEST
+        type: FETCH_DETAIL_REQUEST,
+        filter,
     }
 }
 
-export const fetchDetailSuccess = results => {
+export const fetchDetailSuccess = (results, filter) => {
     return {
         type: FETCH_DETAIL_SUCCESS,
-        payload: results
+        payload: results,
+        filter
     }
 }
 
-export const fetchDeatilFailure = err => {
+export const fetchDetailFailure = (error, filter) => {
     return {
         type: FETCH_DETAIL_FAILURE,
-        paload: err
+        payload: error,
+        filter
+    }
+}
+
+const fetchDetailCredit = (movieId, filter) => {
+    return (dispatch) => {
+        dispatch(fetchDetailRequest(filter))
+        axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${API_KEY}&language=ko-KR`)
+            .then(response => {
+                dispatch(fetchDetailSuccess(response.data, filter))
+            })
+            .catch(error => {
+                dispatch(fetchDetailFailure(error.message, filter))
+            })
+    }
+}
+const fetchDetailVideo = (movieId, filter) => {
+    return (dispatch) => {
+        dispatch(fetchDetailRequest(filter))
+        axios.get(`https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}&language=ko-KR`)
+            .then(response => {
+                dispatch(fetchDetailSuccess(response.data, filter))
+            })
+            .catch(error => {
+                dispatch(fetchDetailFailure(error.message, filter))
+            })
+    }
+}
+const fetchDetailRecommendation = (movieId, filter) => {
+    return (dispatch) => {
+        dispatch(fetchDetailRequest(filter))
+        axios.get(`https://api.themoviedb.org/3/movie/${movieId}/recommendations?api_key=${API_KEY}&language=ko-KR&page=1`)
+            .then(response => {
+                dispatch(fetchDetailSuccess(response.data, filter))
+            })
+            .catch(error => {
+                dispatch(fetchDetailFailure(error.message, filter))
+            })
     }
 }
 
 export const fetchDetail = (movieId) => {
     return (dispatch) =>  {
-        axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=ko-KR`)
+        dispatch(fetchDetailCredit(movieId, 'credit'))
+        dispatch(fetchDetailVideo(movieId, 'video'))
+        dispatch(fetchDetailRecommendation(movieId, 'recommendation'))
+        dispatch(fetchDetailRequest())
+        const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=ko-KR` 
+
+        axios.get(url)
             .then(response => {
                 dispatch(fetchDetailSuccess(response.data))
             })
             .catch(error => {
-                dispatch(fetchDeatilFailure(error.message))
+                dispatch(fetchDetailFailure(error.message))
             })
     }
 }
@@ -43,31 +89,164 @@ export const fetchDetail = (movieId) => {
 // reducer
 const initialState = {
     // detail page
-    loading: false,
-    err: '',
-    results: [],
+    details: {
+        loading: false,
+        err: '',
+        results: [],
+        success: false,
+    },
+    credit: {
+        loading: false,
+        err: '',
+        results: [],
+        success: false,
+    },
+    video: {
+        loading: false,
+        err: '',
+        results: [],
+        success: false,
+    },
+    recommendation: {
+        loading: false,
+        err: '',
+        results: [],
+        success: false,
+    }
 }
 
 export default function reducer(state = initialState, action) {
     switch(action.type) {
         case FETCH_DETAIL_REQUEST:
-            return {
-                ...state,
-                loading: true
+            if(action.filter === 'credit'){
+                return {
+                    ...state,
+                    credit: {
+                        ...state.credit,
+                        loading: true,
+                    }
+                }
             }
+            if(action.filter === 'video'){
+                return {
+                    ...state,
+                    video: {
+                        ...state.video,
+                        loading: true,
+                    }
+                }
+            }
+            if(action.filter === 'recommendation'){
+                return {
+                    ...state,
+                    recommendation: {
+                        ...state.recommendation,
+                        loading: true,
+                    }
+                }
+            }
+            else{
+                return {
+                    ...state,
+                    details: {
+                        ...state.details,
+                        loading: true,
+                    }
+                }
+            }
+            /* falls through */
         case FETCH_DETAIL_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                results: action.payload
+            if(action.filter === 'credit'){
+                return{
+                    ...state,
+                    credit: {
+                        ...state.credit,
+                        loading: false,
+                        results: action.payload,
+                        success: true
+                    }
+                }
             }
+            if(action.filter === 'video'){
+                return{
+                    ...state,
+                    video: {
+                        ...state.video,
+                        loading: false,
+                        results: action.payload,
+                        success: true
+                    }
+                }
+            }
+            if(action.filter === 'recommendation'){
+                return{
+                    ...state,
+                    recommendation: {
+                        ...state.recommendation,
+                        loading: false,
+                        results: action.payload,
+                        success: true
+                    }
+                }
+            }
+            else{
+                return{
+                    ...state,
+                    details: {
+                        ...state.details,
+                        loading: false,
+                        results: action.payload,
+                        success: true
+                    }
+                }
+            }
+            /* falls through */
         case FETCH_DETAIL_FAILURE:
-            return {
-                ...state,
-                loading: false,
-                results: [],
-                err: action.payload
+            if(action.filter === 'credit'){
+                return {
+                    ...state,
+                    credit: {
+                        ...state.credit,
+                        loading: false,
+                        results: [],
+                        err: action.payload
+                    }
+                }
             }
+            if(action.filter === 'video'){
+                return {
+                    ...state,
+                    video: {
+                        ...state.video,
+                        loading: false,
+                        results: [],
+                        err: action.payload
+                    }
+                }
+            }
+            if(action.filter === 'recommendation'){
+                return {
+                    ...state,
+                    recommendation: {
+                        ...state.recommendation,
+                        loading: false,
+                        results: [],
+                        err: action.payload
+                    }
+                }
+            }
+            else{
+                return {
+                    ...state,
+                    details: {
+                        ...state.details,
+                        loading: false,
+                        results: [],
+                        err: action.payload
+                    }
+                }
+            }
+            /* falls through */
         default:
             return state
     }
