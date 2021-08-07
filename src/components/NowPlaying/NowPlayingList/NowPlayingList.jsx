1f46172db09a35dabd9415d56ac88eab1595788b
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-08-07 19:06:06 +0900

    style : formatting

--- src/components/NowPlaying/NowPlayingList/NowPlayingList.jsx
+++ src/components/NowPlaying/NowPlayingList/NowPlayingList.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { GridListTile, GridListTileBar } from '@material-ui/core';
-import * as GS from 'style/componentstyle';
-import { backdropImg, nullImg } from 'utils/constants';
-import { useHistory } from 'react-router-dom';
-import { useGetGridListCols } from 'hooks/useGetGridListCols';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchDetail } from 'store/modules/detail';
+import React from "react";
+import { GridListTile, GridListTileBar } from "@material-ui/core";
+import * as GS from "style/componentstyle";
+import { backdropImg, nullImg } from "utils/constants";
+import { useHistory } from "react-router-dom";
+import { useGetGridListCols } from "hooks/useGetGridListCols";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchDetail } from "store/modules/detail";
 
 const NowPlayingList = () => {
     const cols = useGetGridListCols();
@@ -13,21 +13,32 @@ const NowPlayingList = () => {
     const nowPlaying = useSelector(({ movies }) => movies.nowPlaying.results);
     const history = useHistory();
 
-    return(
-        <>     
-            <GS.ListMovie cellHeight={'auto'} cols={cols} spacing={30}> 
+    return (
+        <>
+            <GS.ListMovie cellHeight={"auto"} cols={cols} spacing={30}>
                 {nowPlaying.results.map((movie) => (
-                    <GridListTile onClick={e => {
-                        dispatch(fetchDetail(e.currentTarget.id))
-                        history.push(`/Detail/${e.currentTarget.id}`)
-                    }} key={movie.id} id={movie.id}>
-                        <img alt={movie.title} src={movie.poster_path === null ? nullImg : (backdropImg + movie.poster_path)}></img>
+                    <GridListTile
+                        onClick={(e) => {
+                            dispatch(fetchDetail(e.currentTarget.id));
+                            history.push(`/Detail/${e.currentTarget.id}`);
+                        }}
+                        key={movie.id}
+                        id={movie.id}
+                    >
+                        <img
+                            alt={movie.title}
+                            src={
+                                movie.poster_path === null
+                                    ? nullImg
+                                    : backdropImg + movie.poster_path
+                            }
+                        ></img>
                         <GridListTileBar title={movie.title}></GridListTileBar>
                     </GridListTile>
                 ))}
             </GS.ListMovie>
         </>
     );
-}
+};
 
 export default NowPlayingList;
