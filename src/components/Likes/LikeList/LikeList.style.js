287f70a67195ba2f26fb4f35af7de41a3b900094
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-08-20 18:05:24 +0900

    삭제버튼 위치 변경

--- src/components/Likes/LikeList/LikeList.style.js
+++ src/components/Likes/LikeList/LikeList.style.js
@@ -1,13 +1,16 @@
-import styled from 'styled-components';
-import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
-import IconButton from '@material-ui/core/IconButton';
+import styled from "styled-components";
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import IconButton from "@material-ui/core/IconButton";
 
 const CustomIconButton = styled(IconButton)`
     color: ${(props) => props.theme.color.fontColor};
+    position: absolute;
+    right: 0;
+    bottom: 4rem;
 `;
 
 const CustomDeleteIcon = styled(DeleteOutlineIcon)`
-
+    font-size: ${(props) => props.theme.fontSize.xxl};
 `;
 
 export { CustomIconButton, CustomDeleteIcon };
