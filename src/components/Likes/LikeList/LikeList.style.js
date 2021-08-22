05d83331fad044287042c5aec3c26e111451fa19
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-08-22 20:00:02 +0900

    삭제 아이콘 위치 변경

--- src/components/Likes/LikeList/LikeList.style.js
+++ src/components/Likes/LikeList/LikeList.style.js
@@ -3,14 +3,11 @@ import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
 import IconButton from "@material-ui/core/IconButton";
 
 const CustomIconButton = styled(IconButton)`
-    color: ${(props) => props.theme.color.fontColor};
-    position: absolute;
-    right: 0;
-    bottom: 4rem;
+    color: ${({ theme }) => theme.color.fontColor};
 `;
 
 const CustomDeleteIcon = styled(DeleteOutlineIcon)`
-    font-size: ${(props) => props.theme.fontSize.xxl};
+    font-size: ${({ theme }) => theme.fontSize.xxl};
 `;
 
 export { CustomIconButton, CustomDeleteIcon };
