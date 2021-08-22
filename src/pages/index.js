78700237ae5b6429c040876f173e0fc7f75c26f0
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-08-22 19:53:10 +0900

    movie page 내보내기

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
-export { default as Detail } from './Detail/Detail';
-export { default as Likes } from './Likes/Likes';
-export { default as Main } from './Main/Main';
-export { default as NowPlaying } from './NowPlaying/NowPlaying';
-export { default as Popular } from './Popular/Popular';
-export { default as Results } from './Results/Results';
-export { default as TopRated } from './TopRated/TopRated';
+export { default as Detail } from "./Detail/Detail";
+export { default as Likes } from "./Likes/Likes";
+export { default as Main } from "./Main/Main";
+export { default as Results } from "./Results/Results";
+export { default as Movie } from "./Movie/Movie";
